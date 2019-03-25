644742a332d155d45ca958f85acd773075587c3e
Mauricio Lauffer <lauffer@outlook.com>
2019-03-25 16:57:53 +1100

    Clear dist folder

--- dist/resources/openui5/password/PasswordRenderer.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["jquery.sap.global","sap/ui/core/Renderer","sap/m/InputBaseRenderer"],function(e,t,r){"use strict";const n=t.extend(r);n.writeInnerAttributes=function(e,t){e.writeAttribute("type","password");if(!t.getEnabled()){e.writeAttribute("readonly","readonly")}};return n},true);
