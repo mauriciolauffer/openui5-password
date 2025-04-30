39c4dfdd5ecc1f78ad11039a9fc1364400deb44b
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-05-01 09:51:02 +1000

    test: Bump wdio-qunit-service

--- test/openui5/password/wdio.conf.mjs
+++ test/openui5/password/wdio.conf.mjs
@@ -14,12 +14,10 @@ export const config = {
   logLevel: 'warn',
   framework: 'mocha',
   reporters: ['spec'],
-  waitforTimeout: 90000,
 
   services: ['qunit'],
 
   mochaOpts: {
     ui: 'bdd',
-    timeout: 90000
   }
 };
